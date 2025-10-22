a18e86ab78b58c9d749b33da0c0b1280ed8f408d
Nasli Minhaj <nasli.minhaj@fincuro.in>
2025-10-22 17:31:32 +0530

    Update party-reference-data.sql

--- party-reference-data.sql
+++ party-reference-data.sql
@@ -1,20 +1,15 @@
 CREATE DATABASE 'prd_directory_db';
 USE `prd_directory_db`;
 
-CREATE TYPE directory_date_type_enum AS ENUM (
-    'OPEN_DATE',
-    'REFRESH_DATE'
-);
-
 
 CREATE TABLE `party_reference_data_directory_entry` (
     `party_reference_data_directory_id` UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     `party_reference` UUID NOT NULL REFERENCES party(party_id),
-    `directory_entry_date_type` directory_date_type_enum NOT NULL,  -- ENUM
+    `directory_entry_date_type` directory_date_type_enum NOT NULL,  
     `directory_entry_date` TIMESTAMPTZ NOT NULL,
-    `party_type`party_type_enum NOT NULL,  -- ENUM
+    `party_type`party_type_enum NOT NULL,  
     `created_at` TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     `updated_at` TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    `status` directory_status_enum DEFAULT 'ACTIVE'  -- ENUM
+    `status` directory_status_enum DEFAULT 'ACTIVE'  
 );
 
