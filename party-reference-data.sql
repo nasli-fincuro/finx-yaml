3d3f5d0e99cbbffb675290308853bfec4a5d74fb
Nasli Minhaj <nasli.minhaj@fincuro.in>
2025-10-22 17:01:58 +0530

    Update party-reference-data.sql

--- party-reference-data.sql
+++ party-reference-data.sql
@@ -12,3 +12,14 @@ CREATE TABLE identification_type (
 	CONSTRAINT identification_type_pkey PRIMARY KEY (identification_type_id),
 	CONSTRAINT identification_type_type_code_key UNIQUE (type_code)
 );
+CREATE TABLE association_obligation_entitlement (
+	obligation_id uuid NOT NULL DEFAULT gen_random_uuid(),
+	association_id uuid NOT NULL,
+	obligation_subject text NULL,
+	obligation_type public.obligation_type_enum NOT NULL,
+	obligation_modality public.obligation_modality_enum NOT NULL,
+	obligation_definition text NULL,
+	created_at timestamptz NULL DEFAULT CURRENT_TIMESTAMP,
+	CONSTRAINT association_obligation_entitlement_pkey PRIMARY KEY (obligation_id),
+	CONSTRAINT association_obligation_entitlement_association_id_fkey FOREIGN KEY (association_id) REFERENCES party_association(association_id)
+);
