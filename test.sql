ec94d8d25178b7ffb120a8df34fc3a8e2940b28f
Nasli Minhaj <nasli.minhaj@fincuro.in>
2025-10-22 19:16:25 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -4,8 +4,8 @@ USE `prd_directory_db`;
 
 DROP TABLE IF EXISTS `party_reference_data_directory_entry`;
 CREATE TABLE `party_reference_data_directory_entry` (
-    `party_reference_data_directory_id` UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    `party_reference` UUID NOT NULL ,
+    `party_reference_data_directory_id` serial PRIMARY KEY ,
+    `party_reference` int NOT NULL ,
     `directory_entry_date_type` varchar(50) NOT NULL,  -- ENUM
     `directory_entry_date` TIMESTAMPTZ NOT NULL,
     `party_type` party_type_enum NOT NULL,  -- ENUM
